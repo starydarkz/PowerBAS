bc02d49a20d417c3f2a086c63a8e5ea328f457bd
Stary <76498661+starydarkz@users.noreply.github.com>
2025-09-05 22:17:10 -0400

    Delete script.ps1

--- script.ps1
+++
@@ -1,9 +0,0 @@
-#ATTACK - PB-T1592.001-01 - Enumerate PlugNPlay Camera
-
-echo "#ATTACK - PB-T1592.001-01 - Enumerate PlugNPlay Camera" | Add-Content -Path PowerBAS.log
-#STEP 0
-$command = Get-CimInstance -Query "SELECT * FROM Win32_PnPEntity WHERE (PNPClass = 'Image' OR PNPClass = 'Camera')"
-#Logging
-echo Request: Get-CimInstance -Query "SELECT * FROM Win32_PnPEntity WHERE (PNPClass = 'Image' OR PNPClass = 'Camera')" | Add-Content -Path PowerBAS.log
-Add-Content -Path PowerBAS.log -Value "Response: '$command'"
-
